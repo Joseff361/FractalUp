eef7c92ab42db9ef3e6277b864b50f0adfb262d1
Joseff361 <joseff361@outlook.com>
2021-04-16 10:37:01 -0500

    p

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -18,7 +18,7 @@ export class SearchComponent implements OnInit {
   ngOnInit(): void {
     DZ.init({
       appId  : '473142',
-      channelUrl : 'http://joseff361.github.io/FractalUp/assets/chanel.html'
+      channelUrl : 'https://joseff361.github.io/FractalUp/assets/chanel.html'
     })
   }
 
