31f13635a9010a9875cdf040c8a858a5d88eaea5
Joseff361 <joseff361@outlook.com>
2021-04-16 12:20:33 -0500

    -s

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -9,7 +9,11 @@ declare var DZ;
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
+
 export class SearchComponent implements OnInit {
+
+  LoginStatus: string = 'login';
 
   constructor(
     private router: Router
@@ -29,12 +33,15 @@ export class SearchComponent implements OnInit {
   }
 
 
-  khe():void{
+  khe(): void{
+
     DZ.login(function(response) {
       if (response.authResponse) {
           console.log('Welcome!  Fetching your information.... ');
           DZ.api('/user/me', function(response) {
               console.log('Good to see you, ' + response.name + '.');
+              document.getElementById("myButton").textContent = response.name;
+              console.log(response);
           });
       } else {
           console.log('User cancelled login or did not fully authorize.');
